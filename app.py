8a0d4091e0cde877e4872daeb76f2225eb72848a
SchmidtDTN <danielpschmidt98@gmail.com>
2022-07-22 15:57:20 -0400

    Add procfile
    Now Heroku should work :sweat_smile:

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def get_db_connection():
     conn = psycopg2.connect(DATABASE_URL, sslmode='require')
     return conn
 
-app = Flask(__name__)
+loanstreet_api = Flask(__name__)
 # TODOs: 
 # OpenAPI doc
 # Client (Rust + TUI?)
